2d842a69c13345ebb2ee07233c7333aaf57abea1
Ace <mohammed.ashfaq1127@gmail.com>
2023-02-02 18:27:23 +0530

    functioning Div and Mul and Add

--- Calculator/Test/script.js
+++ Calculator/Test/script.js
@@ -1,5 +1,5 @@
 
-let s = '6/2/9/18';
+let s = '90+1000/2*5-50+100/2';
     if(s[s.length-1] == '/' || s[s.length-1] == '*' || s[s.length-1] == '+' || s[s.length-1] == '-'){
         s = s.substring(0, s.length-1);
     }
@@ -10,7 +10,8 @@ let s = '6/2/9/18';
     var tempB = -1;
     var ans = -1;
     var start = -1;
-    var end = -1;
+    var end = -5;
+    // DIVISION
     while(s.indexOf('/') != -1) {
         let i = s.indexOf('/');
         //look for operators in order of / , * , + , -
@@ -36,11 +37,13 @@ let s = '6/2/9/18';
 
         
         //look right
+        console.log(i);
         for(let j = i+1; j < s.length; j++){
+            console.log(j);
             if(j == s.length-1){
-                tempB = s.substring(j,s.length);
+                tempB = s.substring(i+1,s.length);
                 end = -1;
-                console.log(tempB + "this tempB");
+                console.log(tempB + "this tempBE-1");
                 break;
             }
             else if(s[j] == '/' || s[j] == '*' || s[j] == '+' || s[j] == '-'){
@@ -58,139 +61,223 @@ let s = '6/2/9/18';
         //     s = s.substring(0,start) + ans + s.substring(end,s.length);
         //     console.log(s + 'a');
         // }
-        if(start == 0){
+        if(start == 0 && end == -1){
+            s = ans;
+            console.log(s + 'A');
+        }
+        else if(start == 0){
             s = ans + s.substring(end,s.length);
-            console.log(s );
+            console.log(s + 'B');
         }
-        else if(ans > 0 && end == -1){
+        else if(end == -1){
             s = s.substring(0,start) + ans;
-            console.log(s );
-        }
-        else if(ans > 0 && start != 0){
-            s = s.substring(0,start)  + ans + s.substring(end,s.length);
-            console.log(s );
+            console.log(s + 'C' );
         }
         else{
-            s = s.substring(0,start) + ans + s.substring(i,end);
-            console.log(s);
+            s = s.substring(0,start)  + ans + s.substring(end,s.length);
+            console.log(s + 'D');
         }
-        break;
+        
+    }
+    //MULTIPLICATION
+    while(s.indexOf('*') != -1) {
+        let i = s.indexOf('*');
+        //look for operators in order of / , * , + , -
+
+        console.log(i + 'first / pos');
+        //look left
+        for(let j = i-1; j >= 0; j--){
+            if(j == 0){
+                tempA = s.substring(0,i);
+                start = 0;
+                console.log(tempA + 'this tempA');
+                break;
+            }
+            
+            else if(s[j] == '*' || s[j] == '+' || s[j] == '-'){
+                tempA = s.substring(j+1,i);
+                start = j+1; //j+1 coz we want to include op at j
+                console.log(tempA + 'this tempA');
+                break;
+            }
+            
+        }
+
+        
+        //look right
+        for(let j = i+1; j < s.length; j++){
+            if(j == s.length-1){
+                tempB = s.substring(i+1,s.length);
+                end = -1;
+                console.log(tempB + "this tempBE0");
+                break;
+            }
+            else if(s[j] == '*' || s[j] == '+' || s[j] == '-'){
+                tempB = s.substring(i+1,j);
+                end = j;  //not j-1 because last element ignored in substring
+                console.log(tempB + "this tempB");
+                break;
+            }
+        }
+
+        ans = +tempA * +tempB;
+        console.log(ans + 'this ans for mul');
+
+        // if(ans > 0 && start==0){
+        //     s = s.substring(0,start) + ans + s.substring(end,s.length);
+        //     console.log(s + 'a');
+        // }
+        if(start == 0 && end == -1){
+            s = ans;
+            console.log(s + 'A');
+        }
+        else if(start == 0){
+            s = ans + s.substring(end,s.length);
+            console.log(s + 'B');
+        }
+        else if(end == -1){
+            s = s.substring(0,start) + ans;
+            console.log(s + 'C' );
+        }
+        else{
+            s = s.substring(0,start)  + ans + s.substring(end,s.length);
+            console.log(s + 'D');
+        }
+        
+    }
+    // ADDITION
+    while(s.indexOf('+') != -1) {
+        let i = s.indexOf('+');
+        //look for operators in order of / , * , + , -
+
+        console.log(i + 'first + pos');
+        //look left
+        for(let j = i-1; j >= 0; j--){
+            if(j == 0){
+                tempA = s.substring(0,i);
+                start = 0;
+                console.log(tempA + 'this tempAS0');
+                break;
+            }
+            
+            else if(s[j] == '+' || s[j] == '-'){
+                tempA = s.substring(j+1,i);
+                start = j+1; //j+1 coz we want to include op at j
+                console.log(tempA + 'this tempA');
+                break;
+            }
+            
+        }
+
+        
+        //look right
+        for(let j = i+1; j < s.length; j++){
+            if(j == s.length-1){
+                tempB = s.substring(i+1,s.length);
+                end = -1;
+                console.log(tempB + "this tempBE-1");
+                break;
+            }
+            else if(s[j] == '+' || s[j] == '-'){
+                tempB = s.substring(i+1,j);
+                end = j;  //not j-1 because last element ignored in substring
+                console.log(tempB + "this tempB");
+                break;
+            }
+        }
+
+        ans = +tempA + +tempB;
+        console.log(ans + 'this ans for add');
+
+        // if(ans > 0 && start==0){
+        //     s = s.substring(0,start) + ans + s.substring(end,s.length);
+        //     console.log(s + 'a');
+        // }
+        if(start == 0 && end == -1){
+            s = ans;
+            console.log(s + 'A');
+        }
+        else if(start == 0){
+            s = ans + s.substring(end,s.length);
+            console.log(s + 'B');
+        }
+        else if(end == -1){
+            s = s.substring(0,start) + ans;
+            console.log(s + 'C' );
+        }
+        else{
+            s = s.substring(0,start)  + ans + s.substring(end,s.length);
+            console.log(s + 'D');
+        }
+        
     }
 
-    // while(s.indexOf('*') != -1) {
-    //     console.log('mul starts');
-    //     console.log(s);
-    //     let i = s.indexOf('*');
-    //     //look for operators in order of / , * , + , -
+     // SUBTRACTION
+     while(s.indexOf('-') != -1) {
+        let i = s.indexOf('-');
+        //look for operators in order of / , * , + , -
 
-    //     // console.log(i);
-    //     //look left
-    //     for(let j = i-1; j >= 0; j--){
-    //         if(j == 0){
-    //             tempA = s.substring(0,i);
-    //             start = 0;
-    //             break;
-    //         }
+        console.log(i + 'first + pos');
+        //look left
+        for(let j = i-1; j >= 0; j--){
+            if(j == 0){
+                tempA = s.substring(0,i);
+                start = 0;
+                console.log(tempA + 'this tempAS0');
+                break;
+            }
             
-    //         else if(s[j] == '*' || s[j] == '+' || s[j] == '-'){
-    //             tempA = s.substring(j+1,i);
-    //             start = j+1;
-    //             break;
-    //         }
+            else if(s[j] == '+' || s[j] == '-'){
+                tempA = s.substring(j+1,i);
+                start = j+1; //j+1 coz we want to include op at j
+                console.log(tempA + 'this tempA');
+                break;
+            }
             
-    //     }
+        }
+
         
-    //     //look right
-    //     for(let j = i+1; j < s.length; j++){
-    //         if(j == s.length-1){
-    //             tempB = s.substring(j,s.length);
-    //             end = -1;
-    //             break;
-    //         }
-    //         else if(s[j] == '*' || s[j] == '+' || s[j] == '-'){
-    //             tempB = s.substring(i+1,j);
-    //             end = j;  //not j-1 because last element ignored in substring
-    //             break;
-    //         }
-    //     }
+        //look right
+        for(let j = i+1; j < s.length; j++){
+            if(j == s.length-1){
+                tempB = s.substring(i+1,s.length);
+                end = -1;
+                console.log(tempB + "this tempBE-1");
+                break;
+            }
+            else if(s[j] == '+' || s[j] == '-'){
+                tempB = s.substring(i+1,j);
+                end = j;  //not j-1 because last element ignored in substring
+                console.log(tempB + "this tempB");
+                break;
+            }
+        }
 
-    //     ans = +tempA * +tempB;
-    //     console.log(ans);
+        ans = +tempA - +tempB;
+        console.log(ans + 'this ans for add');
 
-    //     if(start == 0){
-    //         s = ans + s.substring(end,s.length);
-    //         console.log(s + 'a');
-    //     }
-    //     else if(ans > 0 && end == -1){
-    //         s = s.substring(0,start) + ans;
-    //         console.log(s + 'b');
-    //     }
-    //     else if(ans > 0 && start != 0){
-    //         s = s.substring(0,start)  + ans + s.substring(end,s.length);
-    //         console.log(s + 'c');
-    //     }
-    //     // else{
-    //     //     s = s.substring(0,start) + ans + s.substring(i,end);
-    //     //     console.log(s + 'd');
-    //     // }
-    // }
-
-
-
-
-
-
-    // while(s.indexOf('*') != -1){
-    //     let i = s.indexOf('*');
-
-    //     //Mul op
-
-    //         //look left
-    //     for(let j = i-1; j >= 0; j--){
-    //         if(j == 0){
-    //             tempA = s.substring(0,i);
-    //             start = 0;
-    //             break;
-                
-    //         }
-    //         else if(s[j] == '+' || s[j] == '-'){
-    //             tempA = s.substring(j+1,i);
-    //             start = j+1+1;
-    //             break;
-    //         }
-            
-    //     }
-    //     console.log('mul');
-    //     console.log(tempA);
-            
-    //     //look right
-    //     for(let j = i+1; j < s.length; j++){
-    //         if(j == s.length-1){
-    //             tempB = s.substring(j,s.length);
-    //             end = s.length-1;
-    //             break;
-    //         }
-    //         else if(s[j] == '*' || s[j] == '+' || s[j] == '-'){
-    //             tempB = s.substring(i+1,j);
-    //             end = j;  //not j-1 because last element ignored in substring
-    //             break;
-    //         }
-    //     }
-    //     console.log(tempB);
-
-    //     ans = +tempA * +tempB;
-    //     console.log(ans);
-    //     if(ans > 0 && start== 0){
-    //         s = ans + s.substring(end,s.length);
-    //     }
-    //     else if(ans > 0 && start != 0){
-    //         s = s.substring(0,start) + '+' + ans + s.substring(end,s.length);
-    //     }
-    //     else{
-    //         s = s.substring(0,start) + '-' + ans + s.substring(i,end);
-    //     }
+        // if(ans > 0 && start==0){
+        //     s = s.substring(0,start) + ans + s.substring(end,s.length);
+        //     console.log(s + 'a');
+        // }
+        if(start == 0 && end == -1){
+            s = ans;
+            console.log(s + 'A');
+        }
+        else if(start == 0){
+            s = ans + s.substring(end,s.length);
+            console.log(s + 'B');
+        }
+        else if(end == -1){
+            s = s.substring(0,start) + ans;
+            console.log(s + 'C' );
+        }
+        else{
+            s = s.substring(0,start)  + ans + s.substring(end,s.length);
+            console.log(s + 'D');
+        }
         
-    //     console.log(s);
-    // }
+    }
+
 
 
